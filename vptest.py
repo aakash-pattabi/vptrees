b4c212545ae5a268c50555340981052dd14c532e
Aakash Pattabi <15pattabia@gmail.com>
2019-05-30 18:42:58 -0700

    CLI interface now interacts with TestHarness, running mult. tests

--- vptest.py
+++ vptest.py
@@ -16,7 +16,6 @@ class Test(object):
 		scan_params["data"] = data
 		tree_params["data"] = data
 		forest_params["data"] = data
-		print("Setting up data structures...")
 		self.estimators = [
 			LinearScan(**scan_params),
 			VPTree(**tree_params), 
@@ -54,7 +53,7 @@ class Test(object):
 		bar.finish()
 
 		total_trees = self.estimators[2].n_estimators
-		trees_to_query = np.unique(np.linspace(1, total_trees, total_trees**0.8).astype(int))
+		trees_to_query = np.unique(np.linspace(1, total_trees, int(total_trees**0.8)).astype(int))
 		print("Testing {} #s of trees/query, checking from {}-{} trees/query".format(
 			len(trees_to_query), trees_to_query[0], trees_to_query[-1])
 		)
@@ -112,12 +111,14 @@ class Test(object):
 		error_meds = [k for __, k in sorted(zip(n_trees, [results[k]["median"] for k in keys if "Error-" in k]))]
 		error_maxs = [k for __, k in sorted(zip(n_trees, [results[k]["max"] for k in keys if "Error-" in k]))]
 		n_trees = sorted(n_trees)
+		half_succeed = n_trees[np.argmin(error_meds)]
 		all_succeed = n_trees[np.argmin(error_maxs)]
 
 		plt.clf()
 		plt.plot(n_trees, times, color = "blue", label = "VPForest")
 		plt.axhline(results["VPTree-Time"], color = "green", label = "VPTree")
 		plt.axhline(results["LinearScan-Time"], color = "red", label = "LinearScan")
+		plt.axvline(half_succeed, color = "cornflowerblue", linestyle = ":", label = "> {} trees => VPF is 0.5 exact".format(half_succeed))
 		plt.axvline(all_succeed, color = "orange", linestyle = ":", label = "> {} trees => VPF is exact".format(all_succeed))
 		plt.xlabel("# of trees in forest")
 		plt.ylabel("Mean query time (s)")
@@ -130,6 +131,7 @@ class Test(object):
 		plt.plot(n_trees, hits, color = "blue", label = "VPForest")
 		plt.axhline(results["VPTree-Hits"], color = "green", label = "VPTree")
 		plt.axhline(self.n, color = "red", label = "LinearScan")
+		plt.axvline(half_succeed, color = "cornflowerblue", linestyle = ":", label = "> {} trees => VPF is 0.5 exact".format(half_succeed))
 		plt.axvline(all_succeed, color = "orange", linestyle = ":", label = "> {} trees => VPF is exact".format(all_succeed))
 		plt.xlabel("# of trees in forest")
 		plt.ylabel("Mean search hits")
@@ -143,6 +145,7 @@ class Test(object):
 		plt.plot(n_trees, error_mins, color = "blue", label = "Best query error")
 		plt.plot(n_trees, error_meds, color = "green", label = "Median query error")
 		plt.plot(n_trees, error_maxs, color = "red", label = "Worst query error")
+		plt.axvline(half_succeed, color = "cornflowerblue", linestyle = ":", label = "> {} trees => VPF is 0.5 exact".format(half_succeed))
 		plt.axvline(all_succeed, color = "orange", linestyle = ":", label = "> {} trees => VPF is exact".format(all_succeed))
 		plt.xlabel("# of trees in forest")
 		plt.ylabel("Rank error (0 is best)")
@@ -151,23 +154,42 @@ class Test(object):
 		plt.tight_layout()
 		plt.savefig(self.save_name + "_error.png")
 
+'''
+Assumes for the time being that all data and queries are _Euclidean_ tables of the form (n x d), where
+[n] is the number of data points and [d] is the dimensionality of each pointer. A query, therefore, takes the
+form of a [d]-length vector. 
+'''
 class TestHarness(object):
 	def __init__(self, data_sequence, scan_params, tree_params, forest_params, save_name):
 		self.n_tests = len(data_sequence)
+		print("Initializing data structures for all tests. This may take a while...")
 		self.tests = [Test(
 				data_sequence[i], 
 				scan_params, 
 				tree_params, 
-				forest_params
+				forest_params, 
+				save_name = save_name + "_{n}x{d}".format(
+					n = data_sequence[i].shape[0], 
+					d = data_sequence[i].shape[1]
+				)
 			) for i in range(self.n_tests)]
 		self.save_name = save_name
 		self.results = None
 
 	def test(self, query_sequence):
 		assert len(query_sequence) == self.n_tests
-		self.results = [self.tests[i].test(query_sequence[i]) for i in range(self.n_tests)]
-		self.save()
+		self.results = []
+		for i in range(self.n_tests):
+			print("\nRunning test [{}/{}]:".format(i+1, self.n_tests))
+			res = self.tests[i].test(query_sequence[i])
+			self.results.append(res)
+		self._save()
 		return self.results
+
+	def plot_results(self):
+		assert self.results
+		for i, t in enumerate(self.tests):
+			t.plot_results(self.results[i])
 
 	def _save(self):
 		assert self.results
@@ -188,13 +210,43 @@ if __name__ == "__main__":
 	}
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--n", type = int, help = "number of data points to generate; currently from a uniform distribution U(0, 1)")
-	parser.add_argument("--d", type = int, help = "dimensionality of each data point, yielding an overall data matrix of shape (n, d)")
-	parser.add_argument("--n_queries", type = int, help = "number of queries to hit each data structure with; results are avg'd over all queries")
-	args = parser.parse_args()
+	parser.add_argument("--n", nargs = "+", type = int, required = True, 
+		help = "Number of data points to generate for each test; currently from a uniform distribution U(0, 1)")
 
-	data = np.random.rand(args.n, args.d)
-	q = [np.random.rand(args.d) for i in range(args.n_queries)]
-	t = Test(data, scan_params, tree_params, forest_params, save_name = "output/{}x{}_Euclidean".format(args.n, args.d))
-	results = t.test(q)
-	t.plot_results(results)
+	parser.add_argument("--d", nargs = "+", type = int, required = True, 
+		help = "Dimensionality of data points for each test, which will be over data of size (n_i, d_i) for (n, d) pair i")
+
+	parser.add_argument("--n_queries", nargs = "+", type = int, required = True, 
+		help = "Number of queries over which to summarize performance statistics; either one integer or one integer _per test_")
+
+	args = parser.parse_args()
+	if len(args.d) != len(args.n):
+		parser.error("The number of arguments passed to --n and --d must be the same! (Each data set has an (n, d) pair.)")
+
+	if len(args.n_queries) != 1 and len(args.n_queries) < len(args.d):
+		parser.error("Either pass one value of --n_queries for _each test_, or one global value for all tests!")
+
+	if len(args.n_queries) == 1:
+		args.n_queries *= len(args.n)
+
+	shapes = zip(args.n, args.d)
+	queries = zip(args.d, args.n_queries)
+
+	data_sequence = [np.random.rand(*shape) for shape in shapes]
+	query_sequence = [[np.random.rand(d) for i in range(n_q)] for d, n_q in queries]
+
+	th = TestHarness(
+			data_sequence = data_sequence, 
+			scan_params = scan_params, 
+			tree_params = tree_params, 
+			forest_params = forest_params, 
+			save_name = "output/Euclidean"
+		)
+	th.test(query_sequence)
+	th.plot_results()
+
+	# data = np.random.rand(args.n, args.d)
+	# q = [np.random.rand(args.d) for i in range(args.n_queries)]
+	# t = Test(data, scan_params, tree_params, forest_params, save_name = "output/{}x{}_Euclidean".format(args.n, args.d))
+	# results = t.test(q)
+	# t.plot_results(results)
